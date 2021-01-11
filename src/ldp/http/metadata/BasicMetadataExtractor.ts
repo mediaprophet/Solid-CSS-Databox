zeroid/community-server

feat: Store target identifier when parsing metadata

src/ldp/http/metadata/BasicMetadataExtractor.ts
@@ -1,5 +1,6 @@
 import type { HttpRequest } from '../../../server/HttpRequest';
 import { RepresentationMetadata } from '../../representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../representation/ResourceIdentifier';
 import { MetadataExtractor } from './MetadataExtractor';
 import type { MetadataParser } from './MetadataParser';
 
@@ -14,9 +15,9 @@ export class BasicMetadataExtractor extends MetadataExtractor {
     this.parsers = parsers;
   }
 
-  public async handle(request: HttpRequest):
+  public async handle({ request, identifier }: { request: HttpRequest; identifier: ResourceIdentifier }):
   Promise<RepresentationMetadata> {
-    const metadata = new RepresentationMetadata();
+    const metadata = new RepresentationMetadata(identifier);
     for (const parser of this.parsers) {
       await parser.parse(request, metadata);
     }

src/ldp/http/metadata/MetadataExtractor.ts
@@ -1,9 +1,10 @@
 import type { HttpRequest } from '../../../server/HttpRequest';
 import { AsyncHandler } from '../../../util/AsyncHandler';
 import type { RepresentationMetadata } from '../../representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../representation/ResourceIdentifier';
 
 /**
  * Parses the metadata of a {@link HttpRequest} into a {@link RepresentationMetadata}.
  */
 export abstract class MetadataExtractor extends
-  AsyncHandler<HttpRequest, RepresentationMetadata> {}
+  AsyncHandler<{ request: HttpRequest; identifier: ResourceIdentifier }, RepresentationMetadata> {}

test/unit/ldp/http/metadata/BasicMetadataExtractor.test.ts
@@ -32,7 +32,11 @@ describe('A BasicMetadataExtractor', (): void => {
   });
 
   it('will add metadata from the parsers.', async(): Promise<void> => {
-    const metadata = await handler.handle({ headers: { aa: 'valA', bb: 'valB' } as any } as HttpRequest);
+    const identifier = { path: 'http://test.com/id' };
+    const metadata = await handler.handle(
+      { identifier, request: { headers: { aa: 'valA', bb: 'valB' } as any } as HttpRequest },
+    );
+    expect(metadata.identifier.value).toBe(identifier.path);
     expect(metadata.getAll(RDF.type).map((term): any => term.value)).toEqual([ 'valA', 'valB' ]);
   });
 });

mediaprophet/Solid-CSS-Databox

src/ldp/http/metadata/BasicMetadataExtractor.ts
@@ -1,5 +1,6 @@
 import type { HttpRequest } from '../../../server/HttpRequest';
 import { RepresentationMetadata } from '../../representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../representation/ResourceIdentifier';
 import { MetadataExtractor } from './MetadataExtractor';
 import type { MetadataParser } from './MetadataParser';
 
@@ -14,9 +15,9 @@ export class BasicMetadataExtractor extends MetadataExtractor {
     this.parsers = parsers;
   }
 
-  public async handle({ request }: { request: HttpRequest }):
+  public async handle({ request, target }: { request: HttpRequest; target: ResourceIdentifier }):
   Promise<RepresentationMetadata> {
-    const metadata = new RepresentationMetadata();
+    const metadata = new RepresentationMetadata(target);
     for (const parser of this.parsers) {
       await parser.parse(request, metadata);
     }

src/ldp/http/metadata/MetadataExtractor.ts
@@ -1,9 +1,10 @@
 import type { HttpRequest } from '../../../server/HttpRequest';
 import { AsyncHandler } from '../../../util/AsyncHandler';
 import type { RepresentationMetadata } from '../../representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../representation/ResourceIdentifier';
 
 /**
  * Parses the metadata of a {@link HttpRequest} into a {@link RepresentationMetadata}.
  */
 export abstract class MetadataExtractor extends
-  AsyncHandler<{ request: HttpRequest }, RepresentationMetadata> {}
+  AsyncHandler<{ request: HttpRequest; target: ResourceIdentifier }, RepresentationMetadata> {}

test/unit/ldp/http/metadata/BasicMetadataExtractor.test.ts
@@ -32,7 +32,11 @@ describe('A BasicMetadataExtractor', (): void => {
   });
 
   it('will add metadata from the parsers.', async(): Promise<void> => {
-    const metadata = await handler.handle({ request: { headers: { aa: 'valA', bb: 'valB' } as any } as HttpRequest });
+    const target = { path: 'http://test.com/id' };
+    const metadata = await handler.handle(
+      { target, request: { headers: { aa: 'valA', bb: 'valB' } as any } as HttpRequest },
+    );
+    expect(metadata.identifier.value).toBe(target.path);
     expect(metadata.getAll(RDF.type).map((term): any => term.value)).toEqual([ 'valA', 'valB' ]);
   });
 });