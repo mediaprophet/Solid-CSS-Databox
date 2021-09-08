joeitu/community-server

feat: Convert IDP input data to JSON

src/identity/interaction/SessionHttpHandler.ts
@@ -1,7 +1,7 @@
 import { NotImplementedHttpError } from '../../util/errors/NotImplementedHttpError';
+import { readableToDict } from '../../util/StreamUtil';
 import { InteractionHandler } from './email-password/handler/InteractionHandler';
 import type { InteractionCompleteResult, InteractionHandlerInput } from './email-password/handler/InteractionHandler';
-import { getFormDataRequestBody } from './util/FormDataUtil';
 
 /**
  * Simple InteractionHttpHandler that sends the session accountId to the InteractionCompleter as webId.
@@ -12,7 +12,7 @@ export class SessionHttpHandler extends InteractionHandler {
       throw new NotImplementedHttpError('Only interactions with a valid session are supported.');
     }
 
-    const { remember } = await getFormDataRequestBody(operation);
+    const { remember } = await readableToDict(operation.body!.data);
     return {
       type: 'complete',
       details: { webId: oidcInteraction.session.accountId, shouldRemember: Boolean(remember) },

src/identity/interaction/email-password/handler/ForgotPasswordHandler.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { ensureTrailingSlash, joinUrl } from '../../../../util/PathUtil';
+import { readableToDict } from '../../../../util/StreamUtil';
 import type { TemplateEngine } from '../../../../util/templates/TemplateEngine';
 import type { EmailSender } from '../../util/EmailSender';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
 import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
@@ -41,7 +41,7 @@ export class ForgotPasswordHandler extends InteractionHandler {
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult<{ email: string }>> {
     try {
       // Validate incoming data
-      const { email } = await getFormDataRequestBody(operation);
+      const { email } = await readableToDict(operation.body!.data);
       assert(typeof email === 'string' && email.length > 0, 'Email required');
 
       await this.resetPassword(email);

src/identity/interaction/email-password/handler/RegistrationHandler.ts
@@ -6,8 +6,8 @@ import type { IdentifierGenerator } from '../../../../pods/generate/IdentifierGe
 import type { PodManager } from '../../../../pods/PodManager';
 import type { PodSettings } from '../../../../pods/settings/PodSettings';
 import { joinUrl } from '../../../../util/PathUtil';
+import { readableToDict } from '../../../../util/StreamUtil';
 import type { OwnershipValidator } from '../../../ownership/OwnershipValidator';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
 import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -186,12 +186,12 @@ export class RegistrationHandler extends InteractionHandler {
    * Parses the input request into a `ParseResult`.
    */
   private async parseInput(operation: Operation): Promise<ParsedInput> {
-    const parsed = await getFormDataRequestBody(operation);
+    const parsed = await readableToDict(operation.body!.data);
     const prefilled: Record<string, string> = {};
     try {
       for (const [ key, value ] of Object.entries(parsed)) {
         assert(!Array.isArray(value), `Unexpected multiple values for ${key}.`);
-        prefilled[key] = value ? value.trim() : '';
+        prefilled[key] = typeof value === 'string' ? value.trim() : value;
       }
       return this.validateInput(prefilled);
     } catch (err: unknown) {

src/identity/interaction/email-password/handler/ResetPasswordHandler.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
+import { readableToDict } from '../../../../util/StreamUtil';
 import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -25,7 +25,7 @@ export class ResetPasswordHandler extends InteractionHandler {
       // Extract record ID from request URL
       const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
       // Validate input data
-      const { password, confirmPassword } = await getFormDataRequestBody(operation);
+      const { password, confirmPassword } = await readableToDict(operation.body!.data);
       assert(
         typeof recordId === 'string' && recordId.length > 0,
         'Invalid request. Open the link from your email again',

src/util/StreamUtil.ts
@@ -37,6 +37,17 @@ export async function readableToQuads(stream: Readable): Promise<Store> {
   return quads;
 }
 
+/**
+ * Interprets the stream as JSON and converts it to a Dict.
+ * @param stream - Stream of JSON data.
+ *
+ * @returns The parsed object.
+ */
+export async function readableToDict(stream: Readable): Promise<NodeJS.Dict<any>> {
+  const body = await readableToString(stream);
+  return JSON.parse(body);
+}
+
 // These error messages usually indicate expected behaviour so should not give a warning.
 // We compare against the error message instead of the code
 // since the second one is from an external library that does not assign an error code.

test/unit/util/StreamUtil.test.ts
@@ -4,8 +4,10 @@ import { Quad, NamedNode, Literal, BlankNode, Store } from 'n3';
 import type { Logger } from '../../../src/logging/Logger';
 import { getLoggerFor } from '../../../src/logging/LogUtil';
 import { isHttpRequest } from '../../../src/server/HttpRequest';
-import { guardedStreamFrom, pipeSafely, transformSafely,
-  readableToString, readableToQuads } from '../../../src/util/StreamUtil';
+import {
+  guardedStreamFrom, pipeSafely, transformSafely,
+  readableToString, readableToQuads, readableToDict,
+} from '../../../src/util/StreamUtil';
 
 jest.mock('../../../src/logging/LogUtil', (): any => {
   const logger: Logger = { warn: jest.fn(), log: jest.fn() } as any;
@@ -44,6 +46,13 @@ describe('StreamUtil', (): void => {
 
       const stream = Readable.from([ quad1, quad2, quad3 ]);
       await expect(readableToQuads(stream)).resolves.toEqual(quads);
+    });
+  });
+
+  describe('#readableToDict', (): void => {
+    it('parses the stream as JSON.', async(): Promise<void> => {
+      const stream = Readable.from('{ "key": "value" }');
+      await expect(readableToDict(stream)).resolves.toEqual({ key: 'value' });
     });
   });
 

mediaprophet/Solid-CSS-Databox

src/identity/interaction/SessionHttpHandler.ts
@@ -1,7 +1,7 @@
 import { NotImplementedHttpError } from '../../util/errors/NotImplementedHttpError';
+import { readJsonStream } from '../../util/StreamUtil';
 import { InteractionHandler } from './email-password/handler/InteractionHandler';
 import type { InteractionCompleteResult, InteractionHandlerInput } from './email-password/handler/InteractionHandler';
-import { getFormDataRequestBody } from './util/FormDataUtil';
 
 /**
  * Simple InteractionHttpHandler that sends the session accountId to the InteractionCompleter as webId.
@@ -12,7 +12,7 @@ export class SessionHttpHandler extends InteractionHandler {
       throw new NotImplementedHttpError('Only interactions with a valid session are supported.');
     }
 
-    const { remember } = await getFormDataRequestBody(operation);
+    const { remember } = await readJsonStream(operation.body!.data);
     return {
       type: 'complete',
       details: { webId: oidcInteraction.session.accountId, shouldRemember: Boolean(remember) },

src/identity/interaction/email-password/handler/ForgotPasswordHandler.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { ensureTrailingSlash, joinUrl } from '../../../../util/PathUtil';
+import { readJsonStream } from '../../../../util/StreamUtil';
 import type { TemplateEngine } from '../../../../util/templates/TemplateEngine';
 import type { EmailSender } from '../../util/EmailSender';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
 import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
@@ -41,7 +41,7 @@ export class ForgotPasswordHandler extends InteractionHandler {
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult<{ email: string }>> {
     try {
       // Validate incoming data
-      const { email } = await getFormDataRequestBody(operation);
+      const { email } = await readJsonStream(operation.body!.data);
       assert(typeof email === 'string' && email.length > 0, 'Email required');
 
       await this.resetPassword(email);

src/identity/interaction/email-password/handler/RegistrationHandler.ts
@@ -6,8 +6,8 @@ import type { IdentifierGenerator } from '../../../../pods/generate/IdentifierGe
 import type { PodManager } from '../../../../pods/PodManager';
 import type { PodSettings } from '../../../../pods/settings/PodSettings';
 import { joinUrl } from '../../../../util/PathUtil';
+import { readJsonStream } from '../../../../util/StreamUtil';
 import type { OwnershipValidator } from '../../../ownership/OwnershipValidator';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
 import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -186,12 +186,12 @@ export class RegistrationHandler extends InteractionHandler {
    * Parses the input request into a `ParseResult`.
    */
   private async parseInput(operation: Operation): Promise<ParsedInput> {
-    const parsed = await getFormDataRequestBody(operation);
+    const parsed = await readJsonStream(operation.body!.data);
     const prefilled: Record<string, string> = {};
     try {
       for (const [ key, value ] of Object.entries(parsed)) {
         assert(!Array.isArray(value), `Unexpected multiple values for ${key}.`);
-        prefilled[key] = value ? value.trim() : '';
+        prefilled[key] = typeof value === 'string' ? value.trim() : value;
       }
       return this.validateInput(prefilled);
     } catch (err: unknown) {

src/identity/interaction/email-password/handler/ResetPasswordHandler.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
-import { getFormDataRequestBody } from '../../util/FormDataUtil';
+import { readJsonStream } from '../../../../util/StreamUtil';
 import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -25,7 +25,7 @@ export class ResetPasswordHandler extends InteractionHandler {
       // Extract record ID from request URL
       const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
       // Validate input data
-      const { password, confirmPassword } = await getFormDataRequestBody(operation);
+      const { password, confirmPassword } = await readJsonStream(operation.body!.data);
       assert(
         typeof recordId === 'string' && recordId.length > 0,
         'Invalid request. Open the link from your email again',

src/util/StreamUtil.ts
@@ -37,6 +37,17 @@ export async function readableToQuads(stream: Readable): Promise<Store> {
   return quads;
 }
 
+/**
+ * Interprets the stream as JSON and converts it to a Dict.
+ * @param stream - Stream of JSON data.
+ *
+ * @returns The parsed object.
+ */
+export async function readJsonStream(stream: Readable): Promise<NodeJS.Dict<any>> {
+  const body = await readableToString(stream);
+  return JSON.parse(body);
+}
+
 // These error messages usually indicate expected behaviour so should not give a warning.
 // We compare against the error message instead of the code
 // since the second one is from an external library that does not assign an error code.

test/unit/util/StreamUtil.test.ts
@@ -4,8 +4,10 @@ import { Quad, NamedNode, Literal, BlankNode, Store } from 'n3';
 import type { Logger } from '../../../src/logging/Logger';
 import { getLoggerFor } from '../../../src/logging/LogUtil';
 import { isHttpRequest } from '../../../src/server/HttpRequest';
-import { guardedStreamFrom, pipeSafely, transformSafely,
-  readableToString, readableToQuads } from '../../../src/util/StreamUtil';
+import {
+  guardedStreamFrom, pipeSafely, transformSafely,
+  readableToString, readableToQuads, readJsonStream,
+} from '../../../src/util/StreamUtil';
 
 jest.mock('../../../src/logging/LogUtil', (): any => {
   const logger: Logger = { warn: jest.fn(), log: jest.fn() } as any;
@@ -44,6 +46,13 @@ describe('StreamUtil', (): void => {
 
       const stream = Readable.from([ quad1, quad2, quad3 ]);
       await expect(readableToQuads(stream)).resolves.toEqual(quads);
+    });
+  });
+
+  describe('#readJsonStream', (): void => {
+    it('parses the stream as JSON.', async(): Promise<void> => {
+      const stream = Readable.from('{ "key": "value" }');
+      await expect(readJsonStream(stream)).resolves.toEqual({ key: 'value' });
     });
   });
 