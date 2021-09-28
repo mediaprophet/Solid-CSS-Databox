rpatil524/community-server

feat: Replace acl specific permissions with generic permissions
This required AuxiliaryStrategy to have a new function
indicating if the auxiliary resource just used its associated resource authorization
or its own.

src/ldp/auxiliary/RoutingAuxiliaryStrategy.ts
@@ -18,6 +18,11 @@ export class RoutingAuxiliaryStrategy extends RoutingAuxiliaryIdentifierStrategy
     super(sources);
   }
 
+  public usesAssociatedAuthorization(identifier: ResourceIdentifier): boolean {
+    const source = this.getMatchingSource(identifier);
+    return source.usesAssociatedAuthorization(identifier);
+  }
+
   public isRequiredInRoot(identifier: ResourceIdentifier): boolean {
     const source = this.getMatchingSource(identifier);
     return source.isRequiredInRoot(identifier);

test/unit/ldp/auxiliary/ComposedAuxiliaryStrategy.test.ts
@@ -24,7 +24,7 @@ describe('A ComposedAuxiliaryStrategy', (): void => {
     validator = {
       handleSafe: jest.fn(),
     } as any;
-    strategy = new ComposedAuxiliaryStrategy(identifierStrategy, metadataGenerator, validator, true);
+    strategy = new ComposedAuxiliaryStrategy(identifierStrategy, metadataGenerator, validator, false, true);
   });
 
   it('calls the AuxiliaryIdentifierStrategy for related calls.', async(): Promise<void> => {
@@ -43,6 +43,10 @@ describe('A ComposedAuxiliaryStrategy', (): void => {
     strategy.isAuxiliaryIdentifier(identifier);
     expect(identifierStrategy.isAuxiliaryIdentifier).toHaveBeenCalledTimes(1);
     expect(identifierStrategy.isAuxiliaryIdentifier).toHaveBeenLastCalledWith(identifier);
+  });
+
+  it('returns the injected value for usesAssociatedAuthorization.', async(): Promise<void> => {
+    expect(strategy.usesAssociatedAuthorization()).toBe(false);
   });
 
   it('returns the injected value for isRequiredInRoot.', async(): Promise<void> => {

test/unit/ldp/auxiliary/RoutingAuxiliaryStrategy.test.ts
@@ -11,6 +11,10 @@ class SimpleSuffixStrategy implements AuxiliaryStrategy {
     this.suffix = suffix;
   }
 
+  public usesAssociatedAuthorization(): boolean {
+    return true;
+  }
+
   public getAuxiliaryIdentifier(identifier: ResourceIdentifier): ResourceIdentifier {
     return { path: `${identifier.path}${this.suffix}` };
   }
@@ -75,6 +79,15 @@ describe('A RoutingAuxiliaryStrategy', (): void => {
     expect(sources[0].addMetadata).toHaveBeenCalledTimes(0);
     expect(sources[1].addMetadata).toHaveBeenCalledTimes(1);
     expect(sources[1].addMetadata).toHaveBeenLastCalledWith(metadata);
+  });
+
+  it('#usesAssociatedAuthorization returns the result of the correct source.', async(): Promise<void> => {
+    sources[0].usesAssociatedAuthorization = jest.fn();
+    sources[1].usesAssociatedAuthorization = jest.fn();
+    strategy.usesAssociatedAuthorization(dummy2Id);
+    expect(sources[0].usesAssociatedAuthorization).toHaveBeenCalledTimes(0);
+    expect(sources[1].usesAssociatedAuthorization).toHaveBeenCalledTimes(1);
+    expect(sources[1].usesAssociatedAuthorization).toHaveBeenLastCalledWith(dummy2Id);
   });
 
   it('#isRequiredInRoot returns the result of the correct source.', async(): Promise<void> => {

test/util/AclHelper.ts
@@ -1,5 +1,6 @@
-import type { ResourceStore, Permissions } from '../../src/';
+import type { ResourceStore } from '../../src/';
 import { BasicRepresentation } from '../../src/';
+import type { AclPermissions } from '../../src/ldp/permissions/AclPermissions';
 
 export class AclHelper {
   public readonly store: ResourceStore;
@@ -11,7 +12,7 @@ export class AclHelper {
   public async setSimpleAcl(
     resource: string,
     options: {
-      permissions: Partial<Permissions>;
+      permissions: AclPermissions;
       agentClass?: 'agent' | 'authenticated';
       agent?: string;
       accessTo?: boolean;
@@ -32,7 +33,7 @@ export class AclHelper {
     ];
 
     for (const perm of [ 'Read', 'Append', 'Write', 'Control' ]) {
-      if (options.permissions[perm.toLowerCase() as keyof Permissions]) {
+      if (options.permissions[perm.toLowerCase() as keyof AclPermissions]) {
         acl.push(`;\n acl:mode acl:${perm}`);
       }
     }

mediaprophet/Solid-CSS-Databox

src/ldp/auxiliary/RoutingAuxiliaryStrategy.ts
@@ -18,6 +18,11 @@ export class RoutingAuxiliaryStrategy extends RoutingAuxiliaryIdentifierStrategy
     super(sources);
   }
 
+  public usesOwnAuthorization(identifier: ResourceIdentifier): boolean {
+    const source = this.getMatchingSource(identifier);
+    return source.usesOwnAuthorization(identifier);
+  }
+
   public isRequiredInRoot(identifier: ResourceIdentifier): boolean {
     const source = this.getMatchingSource(identifier);
     return source.isRequiredInRoot(identifier);

test/unit/ldp/auxiliary/ComposedAuxiliaryStrategy.test.ts
@@ -24,7 +24,7 @@ describe('A ComposedAuxiliaryStrategy', (): void => {
     validator = {
       handleSafe: jest.fn(),
     } as any;
-    strategy = new ComposedAuxiliaryStrategy(identifierStrategy, metadataGenerator, validator, true);
+    strategy = new ComposedAuxiliaryStrategy(identifierStrategy, metadataGenerator, validator, false, true);
   });
 
   it('calls the AuxiliaryIdentifierStrategy for related calls.', async(): Promise<void> => {
@@ -43,6 +43,10 @@ describe('A ComposedAuxiliaryStrategy', (): void => {
     strategy.isAuxiliaryIdentifier(identifier);
     expect(identifierStrategy.isAuxiliaryIdentifier).toHaveBeenCalledTimes(1);
     expect(identifierStrategy.isAuxiliaryIdentifier).toHaveBeenLastCalledWith(identifier);
+  });
+
+  it('returns the injected value for usesOwnAuthorization.', async(): Promise<void> => {
+    expect(strategy.usesOwnAuthorization()).toBe(false);
   });
 
   it('returns the injected value for isRequiredInRoot.', async(): Promise<void> => {

test/unit/ldp/auxiliary/RoutingAuxiliaryStrategy.test.ts
@@ -11,6 +11,10 @@ class SimpleSuffixStrategy implements AuxiliaryStrategy {
     this.suffix = suffix;
   }
 
+  public usesOwnAuthorization(): boolean {
+    return true;
+  }
+
   public getAuxiliaryIdentifier(identifier: ResourceIdentifier): ResourceIdentifier {
     return { path: `${identifier.path}${this.suffix}` };
   }
@@ -75,6 +79,15 @@ describe('A RoutingAuxiliaryStrategy', (): void => {
     expect(sources[0].addMetadata).toHaveBeenCalledTimes(0);
     expect(sources[1].addMetadata).toHaveBeenCalledTimes(1);
     expect(sources[1].addMetadata).toHaveBeenLastCalledWith(metadata);
+  });
+
+  it('#usesOwnAuthorization returns the result of the correct source.', async(): Promise<void> => {
+    sources[0].usesOwnAuthorization = jest.fn();
+    sources[1].usesOwnAuthorization = jest.fn();
+    strategy.usesOwnAuthorization(dummy2Id);
+    expect(sources[0].usesOwnAuthorization).toHaveBeenCalledTimes(0);
+    expect(sources[1].usesOwnAuthorization).toHaveBeenCalledTimes(1);
+    expect(sources[1].usesOwnAuthorization).toHaveBeenLastCalledWith(dummy2Id);
   });
 
   it('#isRequiredInRoot returns the result of the correct source.', async(): Promise<void> => {

test/util/AclHelper.ts
@@ -1,5 +1,6 @@
-import type { ResourceStore, Permission } from '../../src/';
+import type { ResourceStore } from '../../src/';
 import { BasicRepresentation } from '../../src/';
+import type { AclPermission } from '../../src/ldp/permissions/AclPermission';
 
 export class AclHelper {
   public readonly store: ResourceStore;
@@ -11,7 +12,7 @@ export class AclHelper {
   public async setSimpleAcl(
     resource: string,
     options: {
-      permissions: Partial<Permission>;
+      permissions: AclPermission;
       agentClass?: 'agent' | 'authenticated';
       agent?: string;
       accessTo?: boolean;
@@ -32,7 +33,7 @@ export class AclHelper {
     ];
 
     for (const perm of [ 'Read', 'Append', 'Write', 'Control' ]) {
-      if (options.permissions[perm.toLowerCase() as keyof Permission]) {
+      if (options.permissions[perm.toLowerCase() as keyof AclPermission]) {
         acl.push(`;\n acl:mode acl:${perm}`);
       }
     }