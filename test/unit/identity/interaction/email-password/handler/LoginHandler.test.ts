rpatil524/community-server

feat: Store account settings separately
Account settings are stored using the WebID as key.
Reason for using the WebID is that this allows faster access to the settings
in authenticated requests.
A consequence of this is that passwords are now always required during registration,
and that there can only be 1 account per WebID.

test/unit/identity/interaction/email-password/handler/LoginHandler.test.ts
@@ -9,17 +9,18 @@ describe('A LoginHandler', (): void => {
   const webId = 'http://alice.test.com/card#me';
   const email = 'alice@test.email';
   let input: InteractionHandlerInput;
-  let storageAdapter: AccountStore;
+  let accountStore: jest.Mocked<AccountStore>;
   let handler: LoginHandler;
 
   beforeEach(async(): Promise<void> => {
     input = {} as any;
 
-    storageAdapter = {
+    accountStore = {
       authenticate: jest.fn().mockResolvedValue(webId),
+      getSettings: jest.fn().mockResolvedValue({ useIdp: true }),
     } as any;
 
-    handler = new LoginHandler(storageAdapter);
+    handler = new LoginHandler(accountStore);
   });
 
   it('errors on invalid emails.', async(): Promise<void> => {
@@ -46,9 +47,17 @@ describe('A LoginHandler', (): void => {
 
   it('throws an IdpInteractionError if there is a problem.', async(): Promise<void> => {
     input.operation = createPostFormOperation({ email, password: 'password!' });
-    (storageAdapter.authenticate as jest.Mock).mockRejectedValueOnce(new Error('auth failed!'));
+    accountStore.authenticate.mockRejectedValueOnce(new Error('auth failed!'));
     const prom = handler.handle(input);
     await expect(prom).rejects.toThrow('auth failed!');
+    await expect(prom).rejects.toThrow(expect.objectContaining({ prefilled: { email }}));
+  });
+
+  it('throws an error if the account does not have the correct settings.', async(): Promise<void> => {
+    input.operation = createPostFormOperation({ email, password: 'password!' });
+    accountStore.getSettings.mockResolvedValueOnce({ useIdp: false });
+    const prom = handler.handle(input);
+    await expect(prom).rejects.toThrow('This account is not registered for identification');
     await expect(prom).rejects.toThrow(expect.objectContaining({ prefilled: { email }}));
   });
 
@@ -58,7 +67,7 @@ describe('A LoginHandler', (): void => {
       type: 'complete',
       details: { webId, shouldRemember: false },
     });
-    expect(storageAdapter.authenticate).toHaveBeenCalledTimes(1);
-    expect(storageAdapter.authenticate).toHaveBeenLastCalledWith(email, 'password!');
+    expect(accountStore.authenticate).toHaveBeenCalledTimes(1);
+    expect(accountStore.authenticate).toHaveBeenLastCalledWith(email, 'password!');
   });
 });

mediaprophet/Solid-CSS-Databox

test/unit/identity/interaction/email-password/handler/LoginHandler.test.ts
@@ -9,42 +9,44 @@ describe('A LoginHandler', (): void => {
   const webId = 'http://alice.test.com/card#me';
   const email = 'alice@test.email';
   let input: InteractionHandlerInput;
-  let storageAdapter: AccountStore;
+  let accountStore: jest.Mocked<AccountStore>;
   let handler: LoginHandler;
 
   beforeEach(async(): Promise<void> => {
     input = {} as any;
 
-    storageAdapter = {
+    accountStore = {
       authenticate: jest.fn().mockResolvedValue(webId),
+      getSettings: jest.fn().mockResolvedValue({ useIdp: true }),
     } as any;
 
-    handler = new LoginHandler(storageAdapter);
+    handler = new LoginHandler(accountStore);
   });
 
   it('errors on invalid emails.', async(): Promise<void> => {
     input.operation = createPostJsonOperation({});
-    let prom = handler.handle(input);
-    await expect(prom).rejects.toThrow('Email required');
+    await expect(handler.handle(input)).rejects.toThrow('Email required');
     input.operation = createPostJsonOperation({ email: [ 'a', 'b' ]});
-    prom = handler.handle(input);
-    await expect(prom).rejects.toThrow('Email required');
+    await expect(handler.handle(input)).rejects.toThrow('Email required');
   });
 
   it('errors on invalid passwords.', async(): Promise<void> => {
     input.operation = createPostJsonOperation({ email });
-    let prom = handler.handle(input);
-    await expect(prom).rejects.toThrow('Password required');
+    await expect(handler.handle(input)).rejects.toThrow('Password required');
     input.operation = createPostJsonOperation({ email, password: [ 'a', 'b' ]});
-    prom = handler.handle(input);
-    await expect(prom).rejects.toThrow('Password required');
+    await expect(handler.handle(input)).rejects.toThrow('Password required');
   });
 
   it('throws an error if there is a problem.', async(): Promise<void> => {
     input.operation = createPostJsonOperation({ email, password: 'password!' });
-    (storageAdapter.authenticate as jest.Mock).mockRejectedValueOnce(new Error('auth failed!'));
-    const prom = handler.handle(input);
-    await expect(prom).rejects.toThrow('auth failed!');
+    accountStore.authenticate.mockRejectedValueOnce(new Error('auth failed!'));
+    await expect(handler.handle(input)).rejects.toThrow('auth failed!');
+  });
+
+  it('throws an error if the account does not have the correct settings.', async(): Promise<void> => {
+    input.operation = createPostJsonOperation({ email, password: 'password!' });
+    accountStore.getSettings.mockResolvedValueOnce({ useIdp: false });
+    await expect(handler.handle(input)).rejects.toThrow('This server is not an identity provider for this account.');
   });
 
   it('returns an InteractionCompleteResult when done.', async(): Promise<void> => {
@@ -53,7 +55,7 @@ describe('A LoginHandler', (): void => {
       type: 'complete',
       details: { webId, shouldRemember: false },
     });
-    expect(storageAdapter.authenticate).toHaveBeenCalledTimes(1);
-    expect(storageAdapter.authenticate).toHaveBeenLastCalledWith(email, 'password!');
+    expect(accountStore.authenticate).toHaveBeenCalledTimes(1);
+    expect(accountStore.authenticate).toHaveBeenLastCalledWith(email, 'password!');
   });
 });