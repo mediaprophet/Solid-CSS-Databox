rpatil524/community-server

feat: Always grant control permissions to pod owners

src/index.ts
@@ -13,6 +13,7 @@ export * from './authorization/Authorization';
 export * from './authorization/Authorizer';
 export * from './authorization/AuxiliaryAuthorizer';
 export * from './authorization/DenyAllAuthorizer';
+export * from './authorization/OwnershipAuthorizer';
 export * from './authorization/PathBasedAuthorizer';
 export * from './authorization/WebAclAuthorization';
 export * from './authorization/WebAclAuthorizer';

mediaprophet/Solid-CSS-Databox

src/index.ts
@@ -15,6 +15,7 @@ export * from './authorization/access-checkers/AgentClassAccessChecker';
 export * from './authorization/access-checkers/AgentGroupAccessChecker';
 
 // Authorization
+export * from './authorization/OwnerPermissionReader';
 export * from './authorization/AllStaticReader';
 export * from './authorization/Authorizer';
 export * from './authorization/AuxiliaryReader';