wvdemeer/CommunitySolidServer

feat: Store methods in MethodNotAllowedHttpError

src/util/errors/MethodNotAllowedHttpError.ts
@@ -1,14 +1,32 @@
+import { DataFactory } from 'n3';
+import type { Quad, Quad_Subject } from 'rdf-js';
+import { toNamedTerm, toObjectTerm } from '../TermUtil';
+import { SOLID_ERROR } from '../Vocabularies';
 import type { HttpErrorOptions } from './HttpError';
-import { HttpError } from './HttpError';
+import { generateHttpErrorClass } from './HttpError';
+import quad = DataFactory.quad;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const BaseHttpError = generateHttpErrorClass(405, 'MethodNotAllowedHttpError');
+
 /**
  * An error thrown when data was found for the requested identifier, but is not supported by the target resource.
+ * Can keep track of the methods that are not allowed.
  */
-export class MethodNotAllowedHttpError extends HttpError {
-  public constructor(message?: string, options?: HttpErrorOptions) {
-    super(405, 'MethodNotAllowedHttpError', message, options);
+export class MethodNotAllowedHttpError extends BaseHttpError {
+  public readonly methods: Readonly<string[]>;
+
+  public constructor(methods: string[] = [], message?: string, options?: HttpErrorOptions) {
+    super(message ?? `${methods} are not allowed.`, options);
+    this.methods = methods;
   }
 
-  public static isInstance(error: any): error is MethodNotAllowedHttpError {
-    return HttpError.isInstance(error) && error.statusCode === 405;
+  public generateMetadata(subject: Quad_Subject | string): Quad[] {
+    const term = toNamedTerm(subject);
+    const quads = super.generateMetadata(term);
+    for (const method of this.methods) {
+      quads.push(quad(term, SOLID_ERROR.terms.methodNotAllowed, toObjectTerm(method, true)));
+    }
+    return quads;
   }
 }

test/unit/util/errors/HttpError.test.ts
@@ -23,7 +23,6 @@ describe('HttpError', (): void => {
     [ 'UnauthorizedHttpError', 401, UnauthorizedHttpError ],
     [ 'ForbiddenHttpError', 403, ForbiddenHttpError ],
     [ 'NotFoundHttpError', 404, NotFoundHttpError ],
-    [ 'MethodNotAllowedHttpError', 405, MethodNotAllowedHttpError ],
     [ 'ConflictHttpError', 409, ConflictHttpError ],
     [ 'PreconditionFailedHttpError', 412, PreconditionFailedHttpError ],
     [ 'PayloadHttpError', 413, PayloadHttpError ],
@@ -75,6 +74,41 @@ describe('HttpError', (): void => {
 
     it('sets the details.', (): void => {
       expect(instance.details).toBe(options.details);
+    });
+
+    it('generates metadata.', (): void => {
+      const subject = namedNode('subject');
+      expect(instance.generateMetadata(subject)).toBeRdfIsomorphic([
+        quad(subject, RDF.terms.type, constructor.uri),
+      ]);
+    });
+  });
+
+  // Separate test due to different constructor
+  describe('MethodNotAllowedHttpError', (): void => {
+    const options = {
+      cause: new Error('cause'),
+      errorCode: 'E1234',
+      details: { some: 'detail' },
+    };
+    const instance = new MethodNotAllowedHttpError([ 'GET' ], 'my message', options);
+
+    it('is valid.', async(): Promise<void> => {
+      expect(new MethodNotAllowedHttpError().methods).toHaveLength(0);
+      expect(MethodNotAllowedHttpError.isInstance(instance)).toBe(true);
+      expect(MethodNotAllowedHttpError.uri).toEqualRdfTerm(generateHttpErrorUri(405));
+      expect(instance.name).toBe('MethodNotAllowedHttpError');
+      expect(instance.statusCode).toBe(405);
+      expect(instance.message).toBe('my message');
+      expect(instance.cause).toBe(options.cause);
+      expect(instance.errorCode).toBe(options.errorCode);
+      expect(new MethodNotAllowedHttpError([ 'GET' ]).errorCode).toBe(`H${405}`);
+
+      const subject = namedNode('subject');
+      expect(instance.generateMetadata(subject)).toBeRdfIsomorphic([
+        quad(subject, RDF.terms.type, MethodNotAllowedHttpError.uri),
+        quad(subject, SOLID_ERROR.terms.methodNotAllowed, literal('GET')),
+      ]);
     });
   });
 });

mediaprophet/Solid-CSS-Databox

src/util/errors/MethodNotAllowedHttpError.ts
@@ -1,14 +1,32 @@
+import { DataFactory } from 'n3';
+import type { Quad, Quad_Subject } from 'rdf-js';
+import { toNamedTerm, toObjectTerm } from '../TermUtil';
+import { SOLID_ERROR } from '../Vocabularies';
 import type { HttpErrorOptions } from './HttpError';
-import { HttpError } from './HttpError';
+import { generateHttpErrorClass } from './HttpError';
+import quad = DataFactory.quad;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const BaseHttpError = generateHttpErrorClass(405, 'MethodNotAllowedHttpError');
+
 /**
  * An error thrown when data was found for the requested identifier, but is not supported by the target resource.
+ * Can keep track of the methods that are not allowed.
  */
-export class MethodNotAllowedHttpError extends HttpError {
-  public constructor(message?: string, options?: HttpErrorOptions) {
-    super(405, 'MethodNotAllowedHttpError', message, options);
+export class MethodNotAllowedHttpError extends BaseHttpError {
+  public readonly methods: Readonly<string[]>;
+
+  public constructor(methods: string[] = [], message?: string, options?: HttpErrorOptions) {
+    super(message ?? `${methods} are not allowed.`, options);
+    this.methods = methods;
   }
 
-  public static isInstance(error: any): error is MethodNotAllowedHttpError {
-    return HttpError.isInstance(error) && error.statusCode === 405;
+  public generateMetadata(subject: Quad_Subject | string): Quad[] {
+    const term = toNamedTerm(subject);
+    const quads = super.generateMetadata(term);
+    for (const method of this.methods) {
+      quads.push(quad(term, SOLID_ERROR.terms.disallowedMethod, toObjectTerm(method, true)));
+    }
+    return quads;
   }
 }

test/unit/util/errors/HttpError.test.ts
@@ -23,7 +23,6 @@ describe('HttpError', (): void => {
     [ 'UnauthorizedHttpError', 401, UnauthorizedHttpError ],
     [ 'ForbiddenHttpError', 403, ForbiddenHttpError ],
     [ 'NotFoundHttpError', 404, NotFoundHttpError ],
-    [ 'MethodNotAllowedHttpError', 405, MethodNotAllowedHttpError ],
     [ 'ConflictHttpError', 409, ConflictHttpError ],
     [ 'PreconditionFailedHttpError', 412, PreconditionFailedHttpError ],
     [ 'PayloadHttpError', 413, PayloadHttpError ],
@@ -81,6 +80,34 @@ describe('HttpError', (): void => {
       const subject = namedNode('subject');
       expect(instance.generateMetadata(subject)).toBeRdfIsomorphic([
         quad(subject, SOLID_ERROR.terms.errorResponse, constructor.uri),
+      ]);
+    });
+  });
+
+  // Separate test due to different constructor
+  describe('MethodNotAllowedHttpError', (): void => {
+    const options = {
+      cause: new Error('cause'),
+      errorCode: 'E1234',
+      details: { some: 'detail' },
+    };
+    const instance = new MethodNotAllowedHttpError([ 'GET' ], 'my message', options);
+
+    it('is valid.', async(): Promise<void> => {
+      expect(new MethodNotAllowedHttpError().methods).toHaveLength(0);
+      expect(MethodNotAllowedHttpError.isInstance(instance)).toBe(true);
+      expect(MethodNotAllowedHttpError.uri).toEqualRdfTerm(generateHttpErrorUri(405));
+      expect(instance.name).toBe('MethodNotAllowedHttpError');
+      expect(instance.statusCode).toBe(405);
+      expect(instance.message).toBe('my message');
+      expect(instance.cause).toBe(options.cause);
+      expect(instance.errorCode).toBe(options.errorCode);
+      expect(new MethodNotAllowedHttpError([ 'GET' ]).errorCode).toBe(`H${405}`);
+
+      const subject = namedNode('subject');
+      expect(instance.generateMetadata(subject)).toBeRdfIsomorphic([
+        quad(subject, SOLID_ERROR.terms.errorResponse, MethodNotAllowedHttpError.uri),
+        quad(subject, SOLID_ERROR.terms.disallowedMethod, literal('GET')),
       ]);
     });
   });