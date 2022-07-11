KamiQuasi/CommunitySolidServer

feat: Update PermissionReaders to support new permission interface

test/unit/authorization/UnionPermissionReader.test.ts
@@ -1,10 +1,13 @@
 import { CredentialGroup } from '../../../src/authentication/Credentials';
 import type { PermissionReader, PermissionReaderInput } from '../../../src/authorization/PermissionReader';
+import type { PermissionSet } from '../../../src/authorization/permissions/Permissions';
+import { IdentifierMap } from '../../../src/authorization/permissions/Permissions';
 import { UnionPermissionReader } from '../../../src/authorization/UnionPermissionReader';
+import { compareMaps } from '../../util/Util';
 
 describe('A UnionPermissionReader', (): void => {
-  const input: PermissionReaderInput =
-    { credentials: {}, identifier: { path: 'http://test.com/foo' }, modes: new Set() };
+  const identifier = { path: 'http://example.com/foo' };
+  const input: PermissionReaderInput = { credentials: {}, accessMap: new IdentifierMap([[ identifier, new Set() ]]) };
   let readers: jest.Mocked<PermissionReader>[];
   let unionReader: UnionPermissionReader;
 
@@ -12,11 +15,11 @@ describe('A UnionPermissionReader', (): void => {
     readers = [
       {
         canHandle: jest.fn(),
-        handle: jest.fn().mockResolvedValue({}),
+        handle: jest.fn().mockResolvedValue(new IdentifierMap()),
       } as any,
       {
         canHandle: jest.fn(),
-        handle: jest.fn().mockResolvedValue({}),
+        handle: jest.fn().mockResolvedValue(new IdentifierMap()),
       } as any,
     ];
 
@@ -25,33 +28,46 @@ describe('A UnionPermissionReader', (): void => {
 
   it('only uses the results of readers that can handle the input.', async(): Promise<void> => {
     readers[0].canHandle.mockRejectedValue(new Error('bad request'));
-    readers[0].handle.mockResolvedValue({ [CredentialGroup.agent]: { read: true }});
-    readers[1].handle.mockResolvedValue({ [CredentialGroup.agent]: { write: true }});
-    await expect(unionReader.handle(input)).resolves.toEqual({ [CredentialGroup.agent]: { write: true }});
+    readers[0].handle.mockResolvedValue(
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { read: true }}]]),
+    );
+    readers[1].handle.mockResolvedValue(
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { write: true }}]]),
+    );
+    compareMaps(await unionReader.handle(input),
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { write: true }}]]));
   });
 
   it('combines results.', async(): Promise<void> => {
-    readers[0].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: true }, [CredentialGroup.public]: undefined },
-    );
-    readers[1].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }},
-    );
-    await expect(unionReader.handle(input)).resolves.toEqual({
-      [CredentialGroup.agent]: { read: true, write: true },
-      [CredentialGroup.public]: { read: false },
-    });
+    const identifier2 = { path: 'http://example.com/foo2' };
+    const identifier3 = { path: 'http://example.com/foo3' };
+    readers[0].handle.mockResolvedValue(new IdentifierMap([
+      [ identifier, { [CredentialGroup.agent]: { read: true }, [CredentialGroup.public]: undefined }],
+      [ identifier2, { [CredentialGroup.agent]: { write: true }}],
+      [ identifier3, { [CredentialGroup.agent]: { append: false }, [CredentialGroup.public]: { delete: true }}],
+    ]));
+    readers[1].handle.mockResolvedValue(new IdentifierMap<PermissionSet>([
+      [ identifier, { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier2, { [CredentialGroup.public]: { read: false }}],
+    ]));
+    compareMaps(await unionReader.handle(input), new IdentifierMap([
+      [ identifier, { [CredentialGroup.agent]: { read: true, write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier2, { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier3, { [CredentialGroup.agent]: { append: false }, [CredentialGroup.public]: { delete: true }}],
+    ]));
   });
 
   it('merges same fields using false > true > undefined.', async(): Promise<void> => {
-    readers[0].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: true, write: false, append: undefined, create: true, delete: undefined }},
-    );
-    readers[1].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: false, write: true, append: true, create: true, delete: undefined }},
-    );
-    await expect(unionReader.handle(input)).resolves.toEqual({
+    readers[0].handle.mockResolvedValue(new IdentifierMap(
+      [[ identifier,
+        { [CredentialGroup.agent]: { read: true, write: false, append: undefined, create: true, delete: undefined }}]],
+    ));
+    readers[1].handle.mockResolvedValue(new IdentifierMap(
+      [[ identifier, { [CredentialGroup.agent]:
+          { read: false, write: true, append: true, create: true, delete: undefined }}]],
+    ));
+    compareMaps(await unionReader.handle(input), new IdentifierMap([[ identifier, {
       [CredentialGroup.agent]: { read: false, write: false, append: true, create: true },
-    });
+    }]]));
   });
 });

mediaprophet/Solid-CSS-Databox

test/unit/authorization/UnionPermissionReader.test.ts
@@ -1,10 +1,15 @@
 import { CredentialGroup } from '../../../src/authentication/Credentials';
 import type { PermissionReader, PermissionReaderInput } from '../../../src/authorization/PermissionReader';
+import type { PermissionSet } from '../../../src/authorization/permissions/Permissions';
+import { AccessMode } from '../../../src/authorization/permissions/Permissions';
 import { UnionPermissionReader } from '../../../src/authorization/UnionPermissionReader';
+import { IdentifierMap, IdentifierSetMultiMap } from '../../../src/util/map/IdentifierMap';
+import { compareMaps } from '../../util/Util';
 
 describe('A UnionPermissionReader', (): void => {
-  const input: PermissionReaderInput =
-    { credentials: {}, identifier: { path: 'http://test.com/foo' }, modes: new Set() };
+  const identifier = { path: 'http://example.com/foo' };
+  const input: PermissionReaderInput = { credentials: {},
+    requestedModes: new IdentifierSetMultiMap<AccessMode>([[ identifier, AccessMode.read ]]) };
   let readers: jest.Mocked<PermissionReader>[];
   let unionReader: UnionPermissionReader;
 
@@ -12,11 +17,11 @@ describe('A UnionPermissionReader', (): void => {
     readers = [
       {
         canHandle: jest.fn(),
-        handle: jest.fn().mockResolvedValue({}),
+        handle: jest.fn().mockResolvedValue(new IdentifierMap()),
       } as any,
       {
         canHandle: jest.fn(),
-        handle: jest.fn().mockResolvedValue({}),
+        handle: jest.fn().mockResolvedValue(new IdentifierMap()),
       } as any,
     ];
 
@@ -25,33 +30,46 @@ describe('A UnionPermissionReader', (): void => {
 
   it('only uses the results of readers that can handle the input.', async(): Promise<void> => {
     readers[0].canHandle.mockRejectedValue(new Error('bad request'));
-    readers[0].handle.mockResolvedValue({ [CredentialGroup.agent]: { read: true }});
-    readers[1].handle.mockResolvedValue({ [CredentialGroup.agent]: { write: true }});
-    await expect(unionReader.handle(input)).resolves.toEqual({ [CredentialGroup.agent]: { write: true }});
+    readers[0].handle.mockResolvedValue(
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { read: true }}]]),
+    );
+    readers[1].handle.mockResolvedValue(
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { write: true }}]]),
+    );
+    compareMaps(await unionReader.handle(input),
+      new IdentifierMap([[ identifier, { [CredentialGroup.agent]: { write: true }}]]));
   });
 
   it('combines results.', async(): Promise<void> => {
-    readers[0].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: true }, [CredentialGroup.public]: undefined },
-    );
-    readers[1].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }},
-    );
-    await expect(unionReader.handle(input)).resolves.toEqual({
-      [CredentialGroup.agent]: { read: true, write: true },
-      [CredentialGroup.public]: { read: false },
-    });
+    const identifier2 = { path: 'http://example.com/foo2' };
+    const identifier3 = { path: 'http://example.com/foo3' };
+    readers[0].handle.mockResolvedValue(new IdentifierMap([
+      [ identifier, { [CredentialGroup.agent]: { read: true }, [CredentialGroup.public]: undefined }],
+      [ identifier2, { [CredentialGroup.agent]: { write: true }}],
+      [ identifier3, { [CredentialGroup.agent]: { append: false }, [CredentialGroup.public]: { delete: true }}],
+    ]));
+    readers[1].handle.mockResolvedValue(new IdentifierMap<PermissionSet>([
+      [ identifier, { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier2, { [CredentialGroup.public]: { read: false }}],
+    ]));
+    compareMaps(await unionReader.handle(input), new IdentifierMap([
+      [ identifier, { [CredentialGroup.agent]: { read: true, write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier2, { [CredentialGroup.agent]: { write: true }, [CredentialGroup.public]: { read: false }}],
+      [ identifier3, { [CredentialGroup.agent]: { append: false }, [CredentialGroup.public]: { delete: true }}],
+    ]));
   });
 
   it('merges same fields using false > true > undefined.', async(): Promise<void> => {
-    readers[0].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: true, write: false, append: undefined, create: true, delete: undefined }},
-    );
-    readers[1].handle.mockResolvedValue(
-      { [CredentialGroup.agent]: { read: false, write: true, append: true, create: true, delete: undefined }},
-    );
-    await expect(unionReader.handle(input)).resolves.toEqual({
+    readers[0].handle.mockResolvedValue(new IdentifierMap(
+      [[ identifier,
+        { [CredentialGroup.agent]: { read: true, write: false, append: undefined, create: true, delete: undefined }}]],
+    ));
+    readers[1].handle.mockResolvedValue(new IdentifierMap(
+      [[ identifier, { [CredentialGroup.agent]:
+          { read: false, write: true, append: true, create: true, delete: undefined }}]],
+    ));
+    compareMaps(await unionReader.handle(input), new IdentifierMap([[ identifier, {
       [CredentialGroup.agent]: { read: false, write: false, append: true, create: true },
-    });
+    }]]));
   });
 });